145bf5bae646432ff3b02ffb4f0fb7b55e8911b3
Isoga88 <gabrieleisola@virgilio.it>
2023-12-19 02:04:16 +0100

    191223

--- components/thirdsection.vue
+++ components/thirdsection.vue
@@ -1,24 +1,9 @@
 <template>
-    <section class="flex flex-row w-full h-screen max-md:flex-col-reverse max-md:items-center">
+    <section class="flex flex-row w-full h-screen max-md:flex-col-reverse max-md:items-center max-md:justify-center">
         <div class="w-9/12 flex justify-center items-center relative pl-3">
-            <div ref="galleria" class="grid grid-flow-col scroll-smooth gap-4 overflow-y-auto overscroll-x-contain carrousel">
-                <div class="snap-start flex items-center justify-center card">
-                    <img class="rounded-lg" src="~/assets/img/first-min.png" alt="">
-                </div>
-                <div class="snap-start flex items-center justify-center card ">
-                    <img class="rounded-lg" src="~/assets/img/taglio1.jpg" alt="">
-                </div>
-                <div class="snap-start flex items-center justify-center card">
-                    <img class="rounded-lg" src="~/assets/img/taglio2.jpg" alt="">
-                </div>
-                <div class="snap-start flex items-center justify-center card">
-                    <img class="rounded-lg" src="~/assets/img/taglio3.jpg" alt="">
-                </div>
-                <div class="snap-start flex items-center justify-center rounded-lg card">
-                    <img class="rounded-lg" src="~/assets/img/first-min.png" alt="">
-                </div>
-                <div class="snap-start flex items-center justify-center card">
-                    <img class="rounded-lg" src="~/assets/img/first-min.png" alt="">
+            <div @mouseover="stopScroll()" @mouseleave="startScroll()" ref="gallery" class="grid grid-flow-col scroll-smooth gap-4 overflow-y-auto overscroll-x-contain carrousel">
+                <div v-for="(element, index) in photos" :key="index" class="snap-center flex items-center justify-center card">
+                    <img class="rounded-lg" :src="element.source" :alt="`${element.name}`">
                 </div>
             </div>
         </div>
@@ -26,14 +11,58 @@
             <h1 class="flex justify-center text-right text-7xl text-amber-500 font-extrabold tracking-wider max-md:text-center portrait:text-center titlefont max-md:w-full">
                 Cosa ci piace
             </h1>
-            <h2 class="text-base font-normal pt-12 text-gray-300 text-right max-md:w-full max-md:px-3 max-md:text-center portrait:px-3 ">
+            <h2 class="text-base font-normal pt-12 text-gray-300 text-right max-md:w-full max-md:px-3 max-md:text-center max-md:pt-3 portrait:px-3  ">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat sunt at temporibus unde vero odit provident architecto omnis consectetur cumque.
             </h2>
         </div>
     </section>
 </template>
-<script>
+<script setup>
+let photos = ref([{source:"first-min.png", name:"Taglio 1"}, {source:"taglio1.jpg", name:"Taglio 2"}, {source:"taglio2.jpg", name:"Taglio 3"}, {source:"taglio3.jpg", name:"Taglio 4"},{source:"first-min.png", name:"Taglio 5"},{source:"first-min.png", name:"Taglio 6"}])
 
+
+const gallery = ref(null)
+const moveLeft = () =>{
+    gallery.value.scrollBy(300,0)
+}
+
+const animateTransition = ref(null)
+let intervalId
+const onScroll = () =>{
+    let windowHeight = window.top.scrollY;
+
+    if(1948 == Math.ceil(windowHeight)){
+        startScroll()
+    }else if(1400 < windowHeight) {
+        animateTransition.value= true
+        stopScroll()
+    }else {
+        animateTransition.value= false
+        stopScroll()
+    }
+}
+
+
+const stopScroll = () =>{
+    if (intervalId) {
+        clearInterval(intervalId)
+        intervalId = null;
+    }
+}
+const startScroll = () =>{
+    if (!intervalId) {
+        intervalId = setInterval(moveLeft, 6000)
+    }
+}
+
+
+onMounted(() => {
+    window.addEventListener('scroll', onScroll);
+});
+
+onBeforeUnmount(()=>{
+    window.removeEventListener('scroll', onScroll)
+})
 </script>
 <style>
 .carrousel{
