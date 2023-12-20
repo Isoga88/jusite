bf75a9e8c8f73786c07febaedc93b7aede9f9978
Isoga88 <gabrieleisola@virgilio.it>
2023-12-20 19:53:30 +0100

    201223

--- components/thirdsection.vue
+++ components/thirdsection.vue
@@ -1,15 +1,15 @@
 <template>
     <section class="flex flex-row w-full h-screen max-md:flex-col-reverse max-md:items-center max-md:justify-center">
-        <div class="w-9/12 flex justify-center items-center relative pl-3 transition-all duration-1000" :class="{ 'bg-opacity-100 translate-x-0 ': animateTransition, 'bg-opacity-0 relative -translate-x-96': !animateTransition }" >
+        <div class="w-9/12 flex justify-center items-center relative pl-3 transition-all duration-1000 max-md:bg-opacity-100 max-md:translate-x-0" :class="{ 'bg-opacity-100 translate-x-0 ': animateTransition, 'bg-opacity-0 relative -translate-x-96': !animateTransition }" >
             <Gallery
             :photos="photos"
             ></Gallery>
         </div>
         <div class="w-3/12 flex flex-col justify-center items-center p-5 max-md:w-full">
-            <h1 class="flex justify-center text-right text-7xl text-amber-500 font-extrabold tracking-wider max-md:text-center portrait:text-center titlefont max-md:w-full transition-all duration-600" :class="{ 'bg-opacity-100 translate-x-0 ': animateTransition, 'bg-opacity-0 relative -translate-x-96': !animateTransition }">
+            <h1 class="flex justify-center text-right text-7xl text-amber-500 font-extrabold tracking-wider max-md:text-center portrait:text-center titlefont max-md:w-full transition-all duration-600 max-md:bg-opacity-100 max-md:translate-x-0" :class="{ 'bg-opacity-100 translate-x-0 ': animateTransition, 'bg-opacity-0 relative -translate-x-96': !animateTransition }">
                 Cosa ci piace
             </h1>
-            <h2 class="text-base font-semibold pt-12 text-gray-300 text-right max-md:w-full max-md:px-3 max-md:text-center max-md:pt-3 portrait:px-3 transition-all duration-500" :class="{ 'bg-opacity-100 translate-x-0 ': animateTransition, 'bg-opacity-0 relative -translate-x-96': !animateTransition }">
+            <h2 class="text-base font-semibold pt-12 text-gray-300 text-right max-md:w-full max-md:px-3 max-md:text-center max-md:pt-3 portrait:px-3 transition-all duration-500 max-md:bg-opacity-100 max-md:translate-x-0" :class="{ 'bg-opacity-100 translate-x-0 ': animateTransition, 'bg-opacity-0 relative -translate-x-96': !animateTransition }">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat sunt at temporibus unde vero odit provident architecto omnis consectetur cumque.
             </h2>
         </div>
